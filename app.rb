c10038dba60c6dbe9879ebdeb2f4cc6ae9da399e
Michael Breedlove <michael.breedlove1@gmail.com>
2014-03-29 01:28:48 -0500

    Added proper thin configuration

--- app.rb
+++ app.rb
@@ -1,29 +1,34 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require 'rqrcode'
+require 'haml'
 
-get '/' do
-  haml :index
-end
-
-get '/ip' do 
-  request.ip
-end
-
-get '/qr/:data' do |d|
-  qr = RQRCode::QRCode.new(d)
-  out = '<style type="text/css"> table{border-width: 0;border-style: none;border-color: #0000ff;border-collapse: collapse;}td{border-width: 0;border-style: none;border-color: #0000ff;border-collapse: collapse;padding: 0;margin: 0;width: 10px;height: 10px;}td.black{background-color: #000;}td.white{background-color: #fff;}</style><table>'
-  qr.modules.each_index do |x|
-    out += '<tr>'
-    qr.modules.each_index do |y|
-        if qr.dark?(x,y)
-          out += '<td class="black"/>'
-        else
-          out += '<td class="white"/>'
-        end
-    end
-    out += '</tr>'
+class AwesomeAPI < Sinatra::Application
+  get '/' do
+    haml :index
   end
-  out += '</table>'
-  out
+
+  get '/ip' do 
+    request.ip
+  end
+
+  get '/qr/:data' do |d|
+    qr = RQRCode::QRCode.new(d)
+    out = '<style type="text/css"> table{border-width: 0;border-style: none;border-color: #0000ff;border-collapse: collapse;}td{border-width: 0;border-style: none;border-color: #0000ff;border-collapse: collapse;padding: 0;margin: 0;width: 10px;height: 10px;}td.black{background-color: #000;}td.white{background-color: #fff;}</style><table>'
+    qr.modules.each_index do |x|
+      out += '<tr>'
+      qr.modules.each_index do |y|
+          if qr.dark?(x,y)
+            out += '<td class="black"/>'
+          else
+            out += '<td class="white"/>'
+          end
+      end
+      out += '</tr>'
+    end
+    out += '</table>'
+    out
+  end
+
+  require_relative 'modules/logger'
 end
